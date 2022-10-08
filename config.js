b86810f9de5f34b4a836c647a5fc3b9dd0b4ef14
Roger Delgado <rogmide@gmail.com>
2022-10-08 09:59:12 -0400

    update secret key

--- config.js
+++ config.js
@@ -8,7 +8,7 @@ const DB_URI = (process.env.NODE_ENV === "test")
   ? "postgresql:///messagely_test"
   : "postgresql:///messagely";
 
-const SECRET_KEY = process.env.SECRET_KEY || "secret";
+const SECRET_KEY = process.env.SECRET_KEY || "fHt$TRcC%25HmE8gC'vD$!Ka%8sOt;~V,XItr+8t+)8;x}O/rSQgGyG7}o^c1')";
 
 const BCRYPT_WORK_FACTOR = 12;
 
